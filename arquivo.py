50349c2aa4a6a378521805b28d72499832ae37d2
Tamires Rodrigues <tamires.rsouza@globo.com>
2021-07-13 16:05:27 -0300

    add pasta teste

---
+++ arquivo.py
@@ -0,0 +1,7 @@
+ idade= int(input('Qual é a sua idade?'))
+
+if idade>=18:
+    print('vc é maior de idade!')
+else:
+   print('vc é menor de idade!)
+
---
+++ scripts/01.py
@@ -0,0 +1,3 @@
+<<<<<<< HEAD
+print('Olá,deu ruim')
+
